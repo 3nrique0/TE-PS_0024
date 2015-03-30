ee3f4604d845ee8686b8673151dc5bee949201de
3nrique0 <3nrique.0@gmail.com>
2015-03-30 19:26:55 +0200

    perl stuff

--- perl_stuff/firstScript.pl
+++ perl_stuff/firstScript.pl
@@ -1,21 +1,29 @@
 #!/usr/bin/perl -w
 
 ## use strict;
-## use warnings;
+use warnings;
 
-my $secretword = "truc";
+%words = qw(
+	fred	camel
+	barney	llama
+	betty	alpaca
+	wilma	alpaca
+);
 
 print ("What's your name?\n");
 $name = <STDIN>;
 chomp ($name);
 
-if ($name eq "patate")
-{
-	print("Hello to you, $name! My vegetable friend !\n");
+if ($name =~ /^randal\b/i) {
+	print("Hello to you, $name! Good of you to be here !\n");
 }
-else
-{
+
+else {
+	$secretword = $words{$name};
 	print("Hello, $name\n");
+	if ($secretword eq "") {
+		$secretword = "groucho";
+	}
 	print("what is the secret word ? ");
 	$guess = <STDIN>;
 	chomp($guess);
