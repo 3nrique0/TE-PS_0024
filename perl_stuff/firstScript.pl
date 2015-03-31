1a0020ee0cf38ecfaa3c30d920b77ab0261b69e4
3nrique0 <3nrique.0@gmail.com>
2015-03-31 14:23:08 +0200

    first module

--- perl_stuff/firstScript.pl
+++ perl_stuff/firstScript.pl
@@ -3,6 +3,59 @@
 ## use strict;
 use warnings;
 
+
+%words = qw(
+	fred	camel
+	barney	llama
+	betty	alpaca
+	wilma	alpaca
+);
+
+
+
+print ("What's your name?\n");
+$name = <STDIN>;
+chomp ($name);
+# $original_name = $name;
+
+# $name =~ s/\W.*//;
+# $name =~ tr/A-Z/a-z/;
+
+
+if ($name =~ /^randal\b/i) {
+	print("Hello to you, $name! Good of you to be here !\n");
+}
+
+else {
+# 	$secretword = $words{$name};
+	print("Hello, $name\n");
+
+# 	if ($secretword eq "") {
+# 		$secretword = "groucho";
+# 	}
+
+	print("what is the secret word ? ");
+	$guess = <STDIN>;
+	chomp($guess);
+
+# 	while ($guess ne $secretword)
+# 	{
+# 		print("$guess is wrong, try gain. What is the secret word? ");
+# 		$guess = <STDIN>;
+# 		chomp($guess);
+# 		
+# 	}
+
+	while (! good_word($name,$guess)){
+		print "Wrongm try agaun. What is the secret word? ";
+		$guess = <STDIN>;
+		chomp($guess) 
+	}
+}
+
+
+
+
 sub good_word {
 	my($somename, $someguess) = @_;		# nomer les paramètres
 	$somename =~ s/\W.*//;			# se debarrasser de tout ce qui suit le premier mot
@@ -17,43 +70,4 @@ sub good_word {
 	}
 }
 
-
-
-
-%words = qw(
-	fred	camel
-	barney	llama
-	betty	alpaca
-	wilma	alpaca
-);
-
-print ("What's your name?\n");
-$name = <STDIN>;
-chomp ($name);
-$original_name = $name;
-$name =~ s/\W.*//;
-$name =~ tr/A-Z/a-z/;
-
-
-if ($name =~ /^randal\b/i) {
-	print("Hello to you, $name! Good of you to be here !\n");
-}
-
-else {
-	$secretword = $words{$name};
-	print("Hello, $name\n");
-	if ($secretword eq "") {
-		$secretword = "groucho";
-	}
-	print("what is the secret word ? ");
-	$guess = <STDIN>;
-	chomp($guess);
-	while ($guess ne $secretword)
-	{
-		print("$guess is wrong, try gain. What is the secret word? ");
-		$guess = <STDIN>;
-		chomp($guess);
-		
-	}
-}
 
